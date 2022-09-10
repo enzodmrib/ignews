ffc4f04692db6a3631dc2d5312cbb2b3861be4d5
enzodmrib <enzorib2@gmail.com>
2022-09-10 06:47:44 -0300

    Installs and configures faunadb, alongside user insertion on database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
-import Providers from 'next-auth/providers'
 import GithubProvider from "next-auth/providers/github"
+
+import { query as q } from 'faunadb'
+import { fauna } from "../../../services/fauna";
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -10,6 +13,42 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+
+        return true;
+      } catch {
+        return false;
+      }
+
+    }
+  }
 }
 
 export default NextAuth(authOptions)
