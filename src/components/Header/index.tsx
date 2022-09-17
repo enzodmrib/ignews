9d13b0c7ab4d1c797359b1e7251a33619397e415
enzodmrib <enzorib2@gmail.com>
2022-09-17 13:31:33 -0300

    Adds posts and preview, and integrates with prismicio

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,9 @@
 import { SignInButton } from '../SignInButton';
+import Link from 'next/link';
+
 import styles from './styles.module.scss'
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
     return (
@@ -7,10 +11,20 @@ export function Header() {
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
+                    <ActiveLink
+                        href="/"
+                        activeClassName={styles.active}
+                    >
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink
+                        href="/posts"
+                        activeClassName={styles.active}
+                    >
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
-                
+
                 <SignInButton />
             </div>
         </header>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,28 @@ import { AppProps } from 'next/app';
 import { Header } from '../components/Header';
 import { SessionProvider as NextAuthProvider } from 'next-auth/react';
 
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { linkResolver, repositoryName } from '../../prismicio';
+
+
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextAuthProvider session={pageProps.session}>
-        <Header />
-        <Component {...pageProps} />
+        <PrismicProvider
+          linkResolver={linkResolver}
+          internalLinkComponent={({ href, ...props }) => (
+            <Link href={href}>
+              <a {...props} />
+            </Link>
+          )}
+        >
+          <Header />
+          <Component {...pageProps} />
+        </PrismicProvider>
       </NextAuthProvider>
     </>
   )
